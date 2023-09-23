80864255e82a6c572429fd13f331e414e901e47a
Mounesh <mouneshknaik@gmail.com>
2023-09-23 14:09:59 +0530

    weekly changes

--- db.php
+++ db.php
@@ -3,17 +3,25 @@ $username="root";
 $password="";
 $host="localhost";
 $database="expense";
+// $username="root";
+// $password="Moni@321";
+// $host="localhost";
+// $database="epiz_32991811_expenses";
+// $username="epiz_32991811";
+// $password="D2B6UzbAabO2WH";
+// $host="sql111.infinityfree.com";
+// $database="epiz_32991811_expenses";
 // $con=mysql_connect("$host","$username","$password") or die("Server Error");
 // mysql_select_db("$database") or die("Database error");
 $conn = new mysqli($host,
     $username, $password, $database);
-
-// if($con==true)
+// print_r($conn);
+// if($conn==true)
 // {
 //     echo "Success";
 // }
 // else
 // {
-//     mysql_close($con);
+//     echo($conn);
 // }
 ?>
